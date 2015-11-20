e64ed866db6190e75b613afb7e0aa92f88c8b8af
flameworks <dewei06@hotmail.com>
2015-11-20 10:21:02 +0800

    added comments

--- interview.py
+++ interview.py
@@ -3,18 +3,20 @@ def interview(num):
 	starting_num = 0
 	while len(answers) < num:
 	# while True:
-		string = str(starting_num)
-		string_length = len(string)
-		if string_length%2 != 0:
-			starting_num*= 10
+		starting_num_str = str(starting_num)
+		starting_num_length = len(starting_num_str)
+		if starting_num_length%2 != 0:
+			starting_num*= 10 #skips all numbers that have odd number of digits
 		else:
-			num1 = string[:string_length/2]
-			num2 = string[string_length/2:]
+			num1 = starting_num_str[:starting_num_length/2]
+			num2 = starting_num_str[starting_num_length/2:]
 			middle_sum = int(num1) + int(num2)
-			if len(str(middle_sum))%2 == 0:
-				middle_starting_pos = (string_length-len(str(middle_sum)))/2
-				if middle_sum == int(string[middle_starting_pos:(string_length-middle_starting_pos)]):
-					answers.append(string)
-					print string
+			middle_num_str = str(middle_sum)
+			middle_num_length = len(middle_num_str)
+			if middle_num_length%2 == 0: #only finding sums that have even number of digits
+				middle_starting_pos = (starting_num_length - middle_num_length)/2
+				if middle_sum == int(starting_num_str[middle_starting_pos:(starting_num_length-middle_starting_pos)]): #right in the middle
+					answers.append(starting_num_str) #optional consolidation into array
+					print starting_num_str
 		starting_num += 1
 	return answers
